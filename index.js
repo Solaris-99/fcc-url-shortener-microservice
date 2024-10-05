617b26e448918afc9c9622fb9bb66622b9971e27
Emanuel Cifuentes <e.cifuentes996@gmail.com>
2024-10-05 16:12:04 +0000

    init: added project files

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const dns = require('node:dns')
 const app = express();
+const urls = ['https://freeCodeCamp.org'];
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+const bParser = bodyParser.urlencoded({extended: false});
 
 app.use(cors());
 
@@ -15,9 +19,40 @@ app.get('/', function(req, res) {
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/shorturl/:url', function(req, res) {
+  
+  const ind = parseInt(req.params.url);
+  if(isNaN(ind)){
+    res.json({error: "Wrong format"})
+  }
+
+  if ( !urls[ind]){
+    res.json({error: "No short URL found for the given input"})
+  }
+
+  res.redirect(urls[ind])
 });
+
+app.post('/api/shorturl', bParser)
+
+app.post('/api/shorturl',(req,res)=>{
+  let url = new URL(req.body.url).hostname
+  dns.lookup(url,(err,add)=>{
+    if(err) return res.json({ error: 'invalid url' });
+    if (!urls.includes(req.body.url)){urls.push(req.body.url)}
+    const ind = urls.indexOf(req.body.url)
+    if (ind >= 0){
+      payload = {
+        original_url: req.body.url,
+        short_url: ind
+      }  
+    }
+  
+    res.json(payload)
+    
+  })
+
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
